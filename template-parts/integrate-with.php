aabdcc66fa6e867f82828acf74ef3ccc8494de61
hnikoloski <h.nikoloski@outlook.com>
2022-02-03 15:06:47 +0100

    App integration is a CPT now

--- template-parts/integrate-with.php
+++ template-parts/integrate-with.php
@@ -1,16 +1,38 @@
-<?php if (have_rows('app_integrations')) : ?>
+<?php
+$args = array(
+    'post_type'              => array('projects'),
+    'post_status'            => array('publish'),
+    'posts_per_page' => -1
+);
+
+$query = new WP_Query($args);
+
+if ($query->have_posts()) {
+?>
     <section id="integrate-with">
         <div class="container slick-slider">
-            <?php while (have_rows('app_integrations')) : the_row();
-                $appLogo = get_sub_field('single_app_logo');
-                $appLink = get_sub_field('single_app_link');
+            <?php
+            while ($query->have_posts()) {
+                $query->the_post();
+                $appLogo = get_the_post_thumbnail_url();
+                $appLink = get_the_permalink();
             ?>
                 <div class="single-app">
                     <a href="<?= $appLink ?>" target="_blank" rel="noopener noreferrer">
-                        <img src="<?= $appLogo; ?>" alt="App integration">
+                        <img src="<?= $appLogo; ?>" alt="<?php the_title(); ?>">
                     </a>
                 </div>
-            <?php endwhile; ?>
+            <?php
+            }
+            ?>
         </div>
     </section>
-<?php endif; ?>
+<?php
+} else {
+    // no posts found
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+
+?>
