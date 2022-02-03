2f96669bc9b5d960a9f366bb096c63a8670f501e
hnikoloski <h.nikoloski@outlook.com>
2022-02-03 13:50:28 +0100

    home responsive update

--- src/scripts/sliders.js
+++ src/scripts/sliders.js
@@ -12,7 +12,13 @@ jQuery(document).ready(function ($) {
         swipeToSlide: true,
         autoplay: true,
         autoplaySpeed: 3000,
-        pauseOnHover: true
+        pauseOnHover: true,
+        responsive: [{
+            breakpoint: 501,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
     });
     $('.testemonials-slider').slick({
         rows: 0,
@@ -21,7 +27,14 @@ jQuery(document).ready(function ($) {
         arrows: false,
         dots: false,
         swipeToSlide: true,
-        variableWidth: true
+        variableWidth: true,
+        responsive: [{
+            breakpoint: 501,
+            settings: {
+                variableWidth: false,
+                slidesToShow: 1
+            }
+        }]
     })
     $('#testemonials-section header .next').on('click', function (e) {
         e.preventDefault();
@@ -31,4 +44,19 @@ jQuery(document).ready(function ($) {
         e.preventDefault();
         $('.testemonials-slider').slick('slickPrev');
     });
+
+    if ($(window).width() < 501) {
+        $('#product-long-description .wrapper p.decorator').remove();
+        $('#product-long-description .wrapper').slick({
+            rows: 0,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            swipeToSlide: true,
+            // variableWidth: true
+        })
+
+    }
 });
